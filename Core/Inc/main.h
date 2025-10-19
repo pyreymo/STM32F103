96e3475b5a92e78dc1b6e41749ea77ff7078cc0b
pyreymo <pyreymo@gmail.com>
2025-10-20 00:17:43 +0800

    dht11 support and display

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,6 +59,8 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define LED_Pin GPIO_PIN_13
 #define LED_GPIO_Port GPIOC
+#define DHT11_Pin GPIO_PIN_1
+#define DHT11_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
