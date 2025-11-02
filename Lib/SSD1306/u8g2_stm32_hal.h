68a863e7c891225dcf5fa5416561519d46874d6b
pyreymo <pyreymo@gmail.com>
2025-11-02 21:26:07 +0800

    improve UI layout

--- Lib/SSD1306/u8g2_stm32_hal.h
+++ Lib/SSD1306/u8g2_stm32_hal.h
@@ -1,9 +1,9 @@
 /**
-  ******************************************************************************
-  * @file    u8g2_stm32_hal.h
-  * @brief   U8g2 hardware abstraction layer for STM32 HAL
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    u8g2_stm32_hal.h
+ * @brief   U8g2 hardware abstraction layer for STM32 HAL
+ ******************************************************************************
+ */
 
 #ifndef __U8G2_STM32_HAL_H
 #define __U8G2_STM32_HAL_H
@@ -13,15 +13,15 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "i2c.h"
 #include "stm32f1xx_hal.h"
 #include "u8g2.h"
-#include "i2c.h"
 
 /* Function prototypes -------------------------------------------------------*/
-uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
-uint8_t u8x8_gpio_and_delay_stm32(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t* u8x8, uint8_t msg, uint8_t arg_int, void* arg_ptr);
+uint8_t u8x8_gpio_and_delay_stm32(u8x8_t* u8x8, uint8_t msg, uint8_t arg_int, void* arg_ptr);
 
-void u8g2_Setup_ssd1306_i2c_128x64_noname_f_hal(u8g2_t *u8g2, const u8g2_cb_t *rotation);
+void u8g2_Setup_ssd1306_i2c_128x64_noname_f_hal(u8g2_t* u8g2, const u8g2_cb_t* rotation);
 
 #ifdef __cplusplus
 }
